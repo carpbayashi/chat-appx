4355cbbac8255c26d353faa924bc61a53b288106
carpbayashi <117645137+carpbayashi@users.noreply.github.com>
2023-01-07 08:38:18 +0900

    画面遷移の実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,6 +1,4 @@
 class RoomUser < ApplicationRecord
-  t.references :room, null: false, foreign_key: true
-  t.references :user, null: false, foreign_key: true
   belongs_to :room
   belongs_to :user
 end
